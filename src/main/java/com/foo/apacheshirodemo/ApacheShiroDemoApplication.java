edbb7a48e3c28191638bf5dec7998d9e0e883e06
liljaewayne <792068007@qq.com>
2018-02-03 17:21:00 +0800

    init commit

---
+++ src/main/java/com/foo/apacheshirodemo/ApacheShiroDemoApplication.java
@@ -0,0 +1,12 @@
+package com.foo.apacheshirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheShiroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheShiroDemoApplication.class, args);
+	}
+}
